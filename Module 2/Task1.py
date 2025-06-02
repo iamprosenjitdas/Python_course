7d1c390626c004bc2cc1f8f7f12b5058b9f4f40e
Prosenjit <dasproison@gmail.com>
2025-06-02 16:04:44 +0530

    Module 2: Added Task 1 and Task 2 files

---
+++ Module 2/Task1.py
@@ -0,0 +1,6 @@
+num1=float(input("Enter the first number"))
+num2=float(input("Enter the second number"))
+print("Addition: \t",num1+num2)
+print("Subtraction: \t",num1-num2)
+print("Multiplication: \t",num1*num2)
+print("Division: \t",num1/num2)
